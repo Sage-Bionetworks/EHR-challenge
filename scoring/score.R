f384023435f3297161fdb053edebb548d00786ec
thomasyu888 <thomasyu888@gmail.com>
2019-11-13 22:53:57 -0800

    Fix

--- scoring/score.R
+++ scoring/score.R
@@ -2,32 +2,8 @@
 suppressPackageStartupMessages(library(argparse))
 suppressPackageStartupMessages(library(jsonlite))
 suppressPackageStartupMessages(library(PRROC))
-
-compute_scores <- function(submission_path, goldstandard_path) {
-  goldstandard <- read.csv(file = 'goldstandard.csv')
-  predictions <- read.csv(file = 'predictions.csv')
-  ​
-  data <- merge(goldstandard, predictions, by="person_id")
-  ​
-  pos <- subset(data, status == 1)
-  neg <- subset(data, status == 0)
-  ​
-  x = pos$score
-  y = neg$score
-  ​
-  roc<-roc.curve(x,y)
-  ​
-  pr <- pr.curve(x,y)
-  
-  c('score_AUC'=roc$auc, 'score_PRAUC'=pr$auc.integral)
-}
-​
-
-​
-#just return roc and pr
-
 parser <- ArgumentParser(description = 'Score submission')
-parser$add_argument('-s', '--submission_file',  type = "character", required = T,
+parser$add_argument('-f', '--submission_file',  type = "character", required = T,
                     help = 'Submission path')
 parser$add_argument('-g', '--goldstandard',  type = "character", required = T,
                     help = 'Goldstandard path')
@@ -37,10 +13,31 @@ parser$add_argument('-s', '--status',  type = "character", required = T,
                     help = 'Submission status')
 args <- parser$parse_args()
 
-if (args$status == "VALIDATED")
-scores = compute_scores(args$submission_file, args$goldstandard)
+compute_scores <- function(submission_path, goldstandard_path) {
+  predictions <- read.csv(submission_path)
+  goldstandard <- read.csv(goldstandard_path)
+  data <- merge(goldstandard, predictions, by="person_id")
+
+  pos <- subset(data, status == 1)
+  neg <- subset(data, status == 0)
+
+  x = pos$score
+  y = neg$score
+
+  roc<-roc.curve(x,y)
+  pr <- pr.curve(x,y)
+  # just return roc and pr
+  c('score_AUC'=roc$auc, 'score_PRAUC'=pr$auc.integral)
+}
+
+if (args$status == "VALIDATED") {
+  scores = compute_scores(args$submission_file, args$goldstandard)
+} else {
+  stop("Invalid submission")
+}
 
 prediction_file_status = "SCORED"
+scores[['prediction_file_status']] = prediction_file_status
 
 result_list = list()
 for (key in names(scores)) {
