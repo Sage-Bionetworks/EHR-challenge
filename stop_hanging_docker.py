0202e4ea2f7d3a3540321230e1300fe2340a5c9a
Thomas Yu <thomasyu888@gmail.com>
2020-01-07 16:32:23 -0800

    Add in graceful killer

--- stop_hanging_docker.py
+++ stop_hanging_docker.py
@@ -2,8 +2,22 @@
 Submissions that are cancelled or exceed time quota aren't
 stopped and removed because docker kill does not send a
 termination signal that can be caught"""
+import signal
+import time
+
 import docker
 import synapseclient
+
+
+class GracefulKiller:
+    """Kill job"""
+    kill_now = False
+    def __init__(self):
+        signal.signal(signal.SIGINT, self.exit_gracefully)
+        signal.signal(signal.SIGTERM, self.exit_gracefully)
+
+    def exit_gracefully(self, signum, frame):
+        self.kill_now = True
 
 
 def stop_hanging_docker_submissions():
@@ -23,4 +37,10 @@ def stop_hanging_docker_submissions():
 
 
 if __name__ == "__main__":
-    stop_hanging_docker_submissions()
+    KILLER = GracefulKiller()
+    while True:
+        stop_hanging_docker_submissions()
+        time.sleep(60)
+        if KILLER.kill_now:
+            break
+    print("End of the program. I was killed gracefully :)")
